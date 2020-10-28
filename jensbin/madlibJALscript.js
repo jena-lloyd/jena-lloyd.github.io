96d231924fffcfea2b7975e7cd5fb8b8c0c6401e
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-10-28 10:45:07 -0400

    changes to js file

--- jensbin/madlibJALscript.js
+++ jensbin/madlibJALscript.js
@@ -1,9 +1,21 @@
 // Jenifer Lloyd
 
 // Input
-
+let adjective = prompt("Enter an adjective: ");
+let noun1 = prompt("Enter a noun: ");
+let name = prompt("Enter a name: ");
+let color = prompt("Enter a color: ");
+let noun2 = prompt("Enter another noun: ");
+let pronoun = prompt("Enter a pronoun: ");
+let song = prompt("Enter a song: ");
+let verb = prompt("Enter a verb: ");
+let pluralNoun = prompt("Enter a plural noun: ");
 
 // Process
-
+let result = "There once was a " + adjective + " " + noun1 +  " named " + name + ", that wanted a " 
++ color + " " + noun2 + ". <br>"
++ pronoun + " would sing " + song + " and " + verb + " on a box. <br>" +
+"The " + pluralNoun + " enjoyed the " + noun1 + "'s holiday performance. <br><br>";
 
 // Output
+alert(result);
