96fc89b9faa9837a3812e8165ad2fe4d30f31f1f
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-10-28 18:20:05 -0400

    still editing the js function

--- jensbin/madlibJALscript.js
+++ jensbin/madlibJALscript.js
@@ -1,22 +1,28 @@
 // Jenifer Lloyd
 
-// Input
-let adjective = prompt("Enter an adjective: ");
-let noun1 = prompt("Enter a noun: ");
-let name = prompt("Enter a name: ");
-let color = prompt("Enter a color: ");
-let noun2 = prompt("Enter another noun: ");
-let pronoun = prompt("Enter a pronoun: ");
-let song = prompt("Enter a song: ");
-let verb = prompt("Enter a verb: ");
-let pluralNoun = prompt("Enter a plural noun: ");
+// Event Listener
+document.getElementById("button").addEventListener("click", madlibJALbuild);
 
-// Process
-let result = "There once was a " + adjective + " " + noun1 +  " named " + name + ", that wanted a " 
-+ color + " " + noun2 + ". " 
-+ pronoun + " would sing " + song + " and " + verb + " on a box. " 
-+ "The " + pluralNoun + " enjoyed the " + noun1 + "'s holiday performance. ";
+//Event Function
+function madlibJALbuild() {
+    // Input
+    let adjective = document.getElementById("adjective").value;
+    let noun1 = document.getElementById("noun1").value;
+    let name = document.getElementById("name").value;
+    let color = document.getElementById("color").value;
+    let noun2 = document.getElementById("noun2").value;
+    let pronoun = document.getElementById("pronoun").value;
+    let song = document.getElementById("song").value;
+    let verb = document.getElementById("verb").value;
+    let pluralNoun = document.getElementById("pluralNoun").value;
 
-// Output
-document.getElementById("result").innerHTML = result;
-alert(result);
+    // Process
+    let result = "There once was a " + adjective + " " + noun1 +  " named " + name + ", that wanted a " 
+    + color + " " + noun2 + ". " 
+    + pronoun + " would sing " + song + " and " + verb + " on a box. " 
+    + "The " + pluralNoun + " enjoyed the " + noun1 + "'s holiday performance. ";
+
+    // Output
+    document.getElementById("result").innerHTML = result;
+}
+
