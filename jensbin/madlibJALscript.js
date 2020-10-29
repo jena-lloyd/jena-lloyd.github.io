137b14b4e32e38670c188faa997fb79a3b74aa37
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-10-28 21:35:03 -0400

    plz fix submit button

--- jensbin/madlibJALscript.js
+++ jensbin/madlibJALscript.js
@@ -1,7 +1,7 @@
 // Jenifer Lloyd
 
 // Event Listener
-document.getElementById("buttonSubmit").addEventListener("click", madlibJALbuild);
+document.getElementById("button").addEventListener("click", madlibJALbuild);
 
 //Event Function
 function madlibJALbuild() {
