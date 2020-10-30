aaca7881a4e58a5df9ceda72bfed8db21f4e06fc
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-10-29 22:36:16 -0400

    testing 2nd function

--- jensbin/madlibJALscript.js
+++ jensbin/madlibJALscript.js
@@ -6,6 +6,7 @@ document.getElementById("button").addEventListener("click", madlibJALbuild);
 //Event Function
 function madlibJALbuild() {
     // Input
+    function convertToUppercase() {
     let adjective = document.getElementById("adjective").value;
     let noun1 = document.getElementById("noun1").value;
     let name = document.getElementById("name").value;
@@ -15,17 +16,15 @@ function madlibJALbuild() {
     let song = document.getElementById("song").value;
     let verb = document.getElementById("verb").value;
     let pluralNoun = document.getElementById("pluralNoun").value;
-
+    }
     // Process
     let result = "There once was a " + adjective + " " + noun1 +  " named " + name + ", that wanted a " 
     + color + " " + noun2 + ". " 
     + pronoun + " would sing " + song + " and " + verb + " on a box. " 
     + "The " + pluralNoun + " enjoyed the " + noun1 + "'s holiday performance. ";
 
-    document.getElementById("play_button").addEventListener("click", reset);
-    
     // Output
     document.getElementById("result").innerHTML = result;
-    
+    document.getElementById("play_button").addEventListener("click", reset);
 }
 
