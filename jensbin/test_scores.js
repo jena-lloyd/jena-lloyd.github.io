82734778716d1cf11a5a851e8e3aef69cc1ac9b0
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-05 05:31:25 -0500

    hero banners addition... idk what i'm doing

--- jensbin/test_scores.js
+++ jensbin/test_scores.js
@@ -7,6 +7,15 @@ var scores = [88, 98, 77, 88];
 var $ = function (id) { return document.getElementById(id); };
 
 var displayResults = $("display_results").innerHTML;
+
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+var element = document.getElementById("results");
+var child = document.getElementById("p1");
+element.insertBefore(para,child);
+
 //var displayScores = $("scores_table").innerHTML;
 //var addScore = $("add")
 
