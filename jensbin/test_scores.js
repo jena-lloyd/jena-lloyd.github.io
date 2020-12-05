65b4d5fed74648ed9e26a9db255aad991ac11ae5
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-05 01:04:36 -0500

    added this files to project bin.

---
+++ jensbin/test_scores.js
@@ -0,0 +1,19 @@
+// Jenifer Lloyd
+// Lab6
+
+var names = ["Ben", "Joel", "Judy", "Anne"];
+var scores = [88, 98, 77, 88];
+
+var $ = function (id) { return document.getElementById(id); };
+
+var displayResults = $("display_results").innerHTML;
+var displayScores = $("scores_table").innerHTML;
+var addScore = $("add")
+
+window.onload = function () {
+	$("add").onclick = addScore;
+	$("display_results").onclick = displayResults;
+	$("display_scores").onclick = displayScores;
+};
+
+
