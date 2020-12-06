a13b5a25b3dc78772c433cfb99607c4051eae1d1
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-06 02:22:39 -0500

    ...what am I even doing

--- jensbin/heroBanners.js
+++ jensbin/heroBanners.js
@@ -1,16 +1,16 @@
-window.onload = rotate;
-var theBanner = 0;
-var heroImages = new
-Array("superhero_banners/superhero1.png","superhero_banners/superhero2.png","superhero_banners/superhero3.png","superhero_banners/superhero4.png","superhero_banners/superhero5.png","superhero_banners/superhero6.png"
-);
-function rotate() {
-theBanner++;
-if (theBanner == heroImages.length) {
-theBanner = 0;
-}
-document.getElementById("image").src = heroImages[theBanner];
-setTimeout(rotate, 3 * 1000);
-}
+//window.onload = rotate;
+//var theBanner = 0;
+//var heroImages = new
+//Array("superhero_banners/superhero1.png","superhero_banners/superhero2.png","superhero_banners/superhero3.png","superhero_banners/superhero4.png","superhero_banners/superhero5.png","superhero_banners/superhero6.png"
+//);
+//function rotate() {
+//theBanner++;
+//if (theBanner == heroImages.length) {
+//theBanner = 0;
+//}
+//document.getElementById("image").src = heroImages[theBanner];
+//setTimeout(rotate, 3 * 1000);
+//}
 
 window.onload = initBannerLink;
 var theBanner = 0;
