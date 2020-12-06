aa6f38da56d2280a67b7605d9544286bb04eca7c
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-06 05:49:14 -0500

    edited my_scripts

--- jensbin/MonsterMash/scripts/my_scripts.js
+++ jensbin/MonsterMash/scripts/my_scripts.js
@@ -1,5 +1,57 @@
 $(document).ready(function(){
-	
-
+    var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0;
+    lighting_one();
+    lighting_two();
+    lighting_three();
+    $("#head").click(function(){
+        if (headclix < 9){
+            $(this).animate({left:"-=367px"},500);
+            headclix += 1;
+        }
+        else{ 
+            $(this).animate({left:"0px"},500);
+            headclix = 0;
+        }
+    });
+    $("#eyes").click(function(){
+        if (eyeclix < 9){
+            $(this).animate({left:"-=367px"},500);
+            eyeclix += 1;
+        }
+        else{ 
+            $(this).animate({left:"0px"},500);
+            eyeclix = 0;
+        }
+    });$("#nose").click(function(){
+        if (noseclix < 9){
+            $(this).animate({left:"-=367px"},500);
+            noseclix += 1;
+        }
+        else{ 
+            $(this).animate({left:"0px"},500);
+            noseclix = 0;
+        }
+    });$("#mouth").click(function(){
+        if (mouthclix < 9){
+            $(this).animate({left:"-=367px"},500);
+            mouthclix += 1;
+        }
+        else{ 
+            $(this).animate({left:"0px"},500);
+            mouthclix = 0;
+        }
+    });
 });//end doc.onready function
 
+function lighting_one(){
+    $("#container #lightning1").fadeIn(250).fadeOut(250);
+    setTimeout("lightning_one()",4000);
+};
+function lighting_two(){
+    $("#container #lightning2").fadeIn("fast").fadeOut("fast");
+    setTimeout("lightning_two()",5000);
+};
+function lighting_three(){
+    $("#container #lightning3").fadeIn("fast").fadeOut("fast");
+    setTimeout("lightning_three()",7000);
+};
