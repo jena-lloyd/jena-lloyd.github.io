3513b31e7b547e221fd5b6fb740565df045a142b
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-09 06:17:11 -0500

    updated

---
+++ jensbin/alien_form.js
@@ -0,0 +1,78 @@
+//Jenifer Lloyd 12.9.2020
+//Alien Form script
+
+//Dialog with Animation
+$( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "clip",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+
+//Datepicker 
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+//Radio Buttons
+$( function() {
+    $( "input" ).checkboxradio({
+      icon: false
+    });
+  } );
+
+//Slider for Weight(.lbs) and Height(ft)
+
+
+//Spinner for number of eyes, arms, and legs
+$( function() {
+    var spinner = $( "#spinner" ).spinner();
+    $( "button" ).button();
+  } );
+
+//Color Picker Slider for Alien color
+$( function() {
+    function hexFromRGB(r, g, b) {
+      var hex = [
+        r.toString( 16 ),
+        g.toString( 16 ),
+        b.toString( 16 )
+      ];
+      $.each( hex, function( nr, val ) {
+        if ( val.length === 1 ) {
+          hex[ nr ] = "0" + val;
+        }
+      });
+      return hex.join( "" ).toUpperCase();
+    }
+    function refreshSwatch() {
+      var red = $( "#red" ).slider( "value" ),
+        green = $( "#green" ).slider( "value" ),
+        blue = $( "#blue" ).slider( "value" ),
+        hex = hexFromRGB( red, green, blue );
+      $( "#swatch" ).css( "background-color", "#" + hex );
+    }
+ 
+    $( "#red, #green, #blue" ).slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshSwatch,
+      change: refreshSwatch
+    });
+    $( "#red" ).slider( "value", 255 );
+    $( "#green" ).slider( "value", 140 );
+    $( "#blue" ).slider( "value", 60 );
+  } );
