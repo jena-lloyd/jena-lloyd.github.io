b785caa69ceeec96400ede73dd084aa70b5ff9d8
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-13 21:20:38 -0500

    removed font-awe file and added 3 files with js

---
+++ portfolio_JAL/scripts/heroBanners.js
@@ -0,0 +1,49 @@
+//window.onload = rotate;
+//var theBanner = 0;
+//var heroImages = new
+//Array("superhero_banners/superhero1.png","superhero_banners/superhero2.png","superhero_banners/superhero3.png","superhero_banners/superhero4.png","superhero_banners/superhero5.png","superhero_banners/superhero6.png"
+//);
+//function rotate() {
+//theBanner++;
+//if (theBanner == heroImages.length) {
+//theBanner = 0;
+//}
+//document.getElementById("image").src = heroImages[theBanner];
+//setTimeout(rotate, 3 * 1000);
+//}
+
+window.onload = initBannerLink;
+var theBanner = 0;
+var banURL = new Array("thechildcenter.com/calling-all-superheroes-and-sidekicks/","cbr.com/best-comic-duos/","cbr.com/marvel-best-worst-superhero-teams/","fashionista.com/2019/02/superhero-costume-design-awards-black-panther-oscars","cbr.com/marvel-vs-dc-who-really-has-the-strongest-heroes/","nationaltoday.com/national-superhero-day/");
+var heroImages = new
+Array("superhero_banners/superhero1.png","superhero_banners/superhero2.png","superhero_banners/superhero3.png","superhero_banners/superhero4.png","superhero_banners/superhero5.png","superhero_banners/superhero6.png"
+);
+
+function initBannerLink() {
+if (document.getElementById("image").parentNode.tagName == "A")
+{
+document.getElementById("image").parentNode.onclick =
+newLocation;
+}
+rotate();
+}
+function newLocation() {
+document.location.href = "http://www." + banURL[theBanner];
+return false;
+}
+function rotate() {
+theBanner++;
+if (theBanner == heroImages.length) {
+theBanner = 0;
+}
+document.getElementById("image").src = heroImages[theBanner];
+setTimeout(rotate, 3 * 1000);
+};
+
+// Attach start and pause event handlers
+   // $("start").onclick = function() {
+        
+   // };
+   // $("pause").onclick = function() {
+        
+   // };
---
+++ portfolio_JAL/scripts/madlibJALscript.js
@@ -0,0 +1,29 @@
+// Jenifer Lloyd
+// Lab4
+
+// Event Listener
+document.getElementById("button").addEventListener("click", madlibJALbuild);
+
+//Event Function
+function madlibJALbuild() {
+    // Input
+    let adjective = document.getElementById("adjective").value;
+    let noun1 = document.getElementById("noun1").value;
+    let name = document.getElementById("name").value;
+    let color = document.getElementById("color").value;
+    let noun2 = document.getElementById("noun2").value;
+    let pronoun = document.getElementById("pronoun").value;
+    let song = document.getElementById("song").value;
+    let verb = document.getElementById("verb").value;
+    let pluralNoun = document.getElementById("pluralNoun").value;
+
+    // Process
+    let result = "There once was a " + adjective + " " + noun1 +  " named " + name + ", that wanted a " 
+    + color + " " + noun2 + ". " 
+    + pronoun + " would sing " + song + " and " + verb + " on a box. " 
+    + "The " + pluralNoun + " enjoyed the " + noun1 + "'s holiday performance. ";
+
+    // Output
+    document.getElementById("result").innerHTML = result;
+}
+
