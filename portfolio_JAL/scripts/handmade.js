1fbdb7e469f51a3bcc12b4febb96729560ce460f
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-13 23:52:31 -0500

    edit

--- portfolio_JAL/scripts/handmade.js
+++ portfolio_JAL/scripts/handmade.js
@@ -15,30 +15,30 @@ setTimeout(rotate, 3 * 1000);
 
 //virus shawl
 window.onload = rotate;
-var theBanner = 0;
-var firstImages = new
+var theBanner2 = 0;
+var secondImages = new
 Array("handmade/myrtle_back.jpg","handmade/myrtle_flat.jpg","handmade/myrtle_close.jpg"
 );
 function rotate() {
-theBanner++;
-if (theBanner == firstImages.length) {
-theBanner = 0;
+theBanner2++;
+if (theBanner2 == secondImages.length) {
+theBanner2 = 0;
 }
-document.getElementById("shawl").src = firstImages[theBanner];
+document.getElementById("shawl").src = secondImages[theBanner2];
 setTimeout(rotate, 3 * 1000);
 }
 
 //punchpop waffle scarf
 window.onload = rotate;
-var theBanner = 0;
-var firstImages = new
+var theBanner3 = 0;
+var thirdImages = new
 Array("handmade/punchpop_waffle.jpg","handmade/punchpop_styled.jpg","handmade/punchpop_close.jpg"
 );
 function rotate() {
-theBanner++;
-if (theBanner == firstImages.length) {
-theBanner = 0;
+theBanner3++;
+if (theBanner3 == thirdImages.length) {
+theBanner3 = 0;
 }
-document.getElementById("waffle").src = firstImages[theBanner];
+document.getElementById("waffle").src = thirdImages[theBanner3];
 setTimeout(rotate, 3 * 1000);
 }
