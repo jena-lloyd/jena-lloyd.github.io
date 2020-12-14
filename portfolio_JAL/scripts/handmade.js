ee02771edc4b1b757e7998422d05b3fa70426094
jena-lloyd <71216119+jena-lloyd@users.noreply.github.com>
2020-12-13 23:48:00 -0500

    cycle 3 maybe

--- portfolio_JAL/scripts/handmade.js
+++ portfolio_JAL/scripts/handmade.js
@@ -1,3 +1,4 @@
+//cherry cola scarf
 window.onload = rotate;
 var theBanner = 0;
 var firstImages = new
@@ -9,5 +10,35 @@ if (theBanner == firstImages.length) {
 theBanner = 0;
 }
 document.getElementById("cherry").src = firstImages[theBanner];
+setTimeout(rotate, 3 * 1000);
+}
+
+//virus shawl
+window.onload = rotate;
+var theBanner = 0;
+var firstImages = new
+Array("handmade/myrtle_back.jpg","handmade/myrtle_flat.jpg","handmade/myrtle_close.jpg"
+);
+function rotate() {
+theBanner++;
+if (theBanner == firstImages.length) {
+theBanner = 0;
+}
+document.getElementById("shawl").src = firstImages[theBanner];
+setTimeout(rotate, 3 * 1000);
+}
+
+//punchpop waffle scarf
+window.onload = rotate;
+var theBanner = 0;
+var firstImages = new
+Array("handmade/punchpop_waffle.jpg","handmade/punchpop_styled.jpg","handmade/punchpop_close.jpg"
+);
+function rotate() {
+theBanner++;
+if (theBanner == firstImages.length) {
+theBanner = 0;
+}
+document.getElementById("waffle").src = firstImages[theBanner];
 setTimeout(rotate, 3 * 1000);
 }
